6e7e1a6ed3187a07cb3fc975906c95d5c51b1e1f
Onizik <onizyk636@gmail.com>
2025-08-03 13:44:30 +0600

    end_game

--- loop_minotaur/scripts/main_game.gd
+++ loop_minotaur/scripts/main_game.gd
@@ -26,4 +26,5 @@ func _process(delta: float) -> void:
 
 
 func _on_biba_timeout() -> void:
-	pass # Replace with function body.
+	$root/menu/anime.play("end_loop")
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
